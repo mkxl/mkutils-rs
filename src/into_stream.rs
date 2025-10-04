abe7cf60c24e750de618861ad5c188dfbb3260ff
michael <66692526+mkxl@users.noreply.github.com>
2025-10-04 12:33:04 -0400

    updates

--- src/into_stream.rs
+++ src/into_stream.rs
@@ -1,6 +1,10 @@
 use futures::Stream;
-use tokio::{sync::mpsc::UnboundedReceiver as UnboundedMpscReceiver, time::Interval};
-use tokio_stream::wrappers::{IntervalStream, UnboundedReceiverStream as UnboundedMpscReceiverStream};
+use tokio::{
+    io::{AsyncBufRead, Lines},
+    sync::mpsc::UnboundedReceiver as UnboundedMpscReceiver,
+    time::Interval,
+};
+use tokio_stream::wrappers::{IntervalStream, LinesStream, UnboundedReceiverStream as UnboundedMpscReceiverStream};
 
 pub trait IntoStream {
     type Stream: Stream;
@@ -13,6 +17,14 @@ impl IntoStream for Interval {
 
     fn into_stream(self) -> Self::Stream {
         IntervalStream::new(self)
+    }
+}
+
+impl<R: AsyncBufRead> IntoStream for Lines<R> {
+    type Stream = LinesStream<R>;
+
+    fn into_stream(self) -> Self::Stream {
+        LinesStream::new(self)
     }
 }
 

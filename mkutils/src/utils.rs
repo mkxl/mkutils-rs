4e9481aa82b9b08de0041f222e583b20cfb6cc70
michael <66692526+mkxl@users.noreply.github.com>
2025-12-07 13:08:37 -0500

    updates

--- mkutils/src/utils.rs
+++ mkutils/src/utils.rs
@@ -6,6 +6,7 @@ use crate::{
     is::Is,
     read_value::ReadValue,
     run_for::RunForError,
+    socket::{Request, Socket},
     status::Status,
 };
 use anyhow::{Context, Error as AnyhowError};
@@ -937,6 +938,10 @@ pub trait Utils {
         Self: AsRef<Path>,
     {
         std::fs::remove_file(self)
+    }
+
+    async fn respond_to<T: Request<Response = Self>>(&self, socket: &mut Socket) -> Result<(), AnyhowError> {
+        socket.respond::<T>(self).await
     }
 
     // TODO-ac2072:

e92bd78dc2412187ec648e023dd3d0288534a455
michael <66692526+mkxl@users.noreply.github.com>
2026-02-18 10:46:07 -0500

    updates

--- mkutils-macros/src/basic.rs
+++ mkutils-macros/src/basic.rs
@@ -9,31 +9,36 @@ use syn::{
 pub struct Basic;
 
 impl Basic {
-    fn field_assignment(field: &Field, method: &Ident) -> Result<TokenStream2, SynError> {
+    fn field_assignment(field: &Field, trait_path: &Path, method: &Ident) -> Result<TokenStream2, SynError> {
         let Some(field_name) = &field.ident else {
             return Err(Error::c_struct_field_missing_name(field.span()));
         };
-        let field_assignment = quote::quote! { #field_name: self.#field_name.#method(&other.#field_name) };
+        let field_assignment =
+            quote::quote! { #field_name: <Self as #trait_path>::#method(&self.#field_name, &other.#field_name) };
 
         Ok(field_assignment)
     }
 
-    fn value_for_c_struct(fields_named: &FieldsNamed, method: &Ident) -> Result<TokenStream2, SynError> {
+    fn value_for_c_struct(
+        fields_named: &FieldsNamed,
+        trait_path: &Path,
+        method: &Ident,
+    ) -> Result<TokenStream2, SynError> {
         let field_assignments = fields_named
             .named
             .iter()
-            .map(|field| Self::field_assignment(field, method))
+            .map(|field| Self::field_assignment(field, trait_path, method))
             .collect::<Result<Vec<TokenStream2>, SynError>>()?;
         let value = quote::quote! { Self { #(#field_assignments),* } };
 
         Ok(value)
     }
 
-    fn value_for_tuple_struct(fields_unnamed: &FieldsUnnamed, method: &Ident) -> TokenStream2 {
+    fn value_for_tuple_struct(fields_unnamed: &FieldsUnnamed, trait_path: &Path, method: &Ident) -> TokenStream2 {
         let num_fields = fields_unnamed.unnamed.len();
         let field_values = (0..num_fields)
             .map(Index::from)
-            .map(|index| quote::quote! { self.#index.#method(&other.#index) });
+            .map(|index| quote::quote! { <Self as #trait_path>::#method(&self.#index, &other.#index) });
 
         quote::quote! { Self(#(#field_values),*) }
     }
@@ -42,13 +47,13 @@ impl Basic {
         quote::quote! { Self }
     }
 
-    fn value(input: &DeriveInput, method: &Ident) -> Result<TokenStream2, SynError> {
+    fn value(input: &DeriveInput, trait_path: &Path, method: &Ident) -> Result<TokenStream2, SynError> {
         let Data::Struct(data_struct) = &input.data else {
             return Err(Error::unsupported_item_type(input.span()));
         };
         let value = match &data_struct.fields {
-            Fields::Named(fields_named) => Self::value_for_c_struct(fields_named, method)?,
-            Fields::Unnamed(fields_unnamed) => Self::value_for_tuple_struct(fields_unnamed, method),
+            Fields::Named(fields_named) => Self::value_for_c_struct(fields_named, trait_path, method)?,
+            Fields::Unnamed(fields_unnamed) => Self::value_for_tuple_struct(fields_unnamed, trait_path, method),
             Fields::Unit => Self::value_for_unit_struct(),
         };
 
@@ -60,7 +65,7 @@ impl Basic {
         let (impl_generics, input_generics, input_where_clause) = input.generics.split_for_impl();
         let trait_path = syn::parse_str::<Path>(trait_path)?;
         let method = syn::parse_str::<Ident>(method)?;
-        let value = Self::value(input, &method)?;
+        let value = Self::value(input, &trait_path, &method)?;
         let impl_block_token_stream = quote::quote! {
             impl #impl_generics #trait_path for #input_ident #input_generics #input_where_clause {
                 fn #method(&self, other: &Self) -> Self {

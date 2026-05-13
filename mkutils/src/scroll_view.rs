84d429dbcd462812dcc44814248efe4c8381383e
michael <66692526+mkxl@users.noreply.github.com>
2026-05-13 19:33:15 -0400

    updates

--- mkutils/src/scroll_view.rs
+++ mkutils/src/scroll_view.rs
@@ -1,22 +1,10 @@
 use crate::{geometry::Orientation, scroll_view_state::ScrollViewState, utils::Utils};
-use ratatui::{Frame, layout::Rect, text::Line as RatatuiLine};
-use std::ops::Range;
+use ratatui::{Frame, layout::Rect};
 
 pub trait ScrollView {
-    fn ratatui_lines(&self, rows: Range<usize>, cols: Range<usize>) -> Vec<RatatuiLine<'_>>;
-
     fn scroll_view_state_mut(&mut self) -> &mut ScrollViewState;
 
-    fn render_content(&mut self, frame: &mut Frame, content_area: Rect) {
-        let scroll_offset = self.scroll_view_state_mut().scroll_offset();
-        let rows = scroll_offset.y.range_from_len(content_area.height.into());
-        let cols = scroll_offset.x.range_from_len(content_area.width.into());
-        let ratatui_line_and_row_area_pairs = self.ratatui_lines(rows, cols).into_iter().zip(content_area.rows());
-
-        for (ratatui_line, row_area) in ratatui_line_and_row_area_pairs {
-            ratatui_line.render_to(frame, row_area);
-        }
-    }
+    fn render_content(&mut self, frame: &mut Frame, content_area: Rect);
 
     fn render_scroll_bars(&mut self, frame: &mut Frame, content_area: Rect) {
         let max_scroll_offset = self.scroll_view_state_mut().max_scroll_offset();
